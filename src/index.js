540629fe6881087fd80746d88ca23afa7f7e8431
Xinran Wang <xw1992@gmail.com>
2017-09-16 12:56:28 -0400

    Add bootstrap and react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
