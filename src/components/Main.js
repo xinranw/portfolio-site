3097541e6de2330ac6f39551533029db3683b795
Xinran Wang <xw1992@gmail.com>
2017-09-22 13:51:51 -0400

    Switch to using home2

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ import Home from "./Home/Home";
 import Home2 from "./Home/Home2";
 import Portfolio from "./Portfolio/Portfolio";
 import Resume from "./Resume/Resume";
-import xinranResume from "./Resume/xinran_wang_resume_2017_digital.pdf";
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -14,8 +13,8 @@ import xinranResume from "./Resume/xinran_wang_resume_2017_digital.pdf";
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/home2" component={Home2} />
+      <Route exact path="/" component={Home2} />
+      <Route exact path="/home1" component={Home} />
       <Route path="/projects" component={Portfolio} />
       <Route path="/resume" component={Resume} />
     </Switch>
