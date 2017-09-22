caca7e75456a9909693d84cce81470f3680b3c95
Xinran Wang <xw1992@gmail.com>
2017-09-22 12:04:42 -0400

    Update production config

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
